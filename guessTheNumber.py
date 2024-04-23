31eb019f9d69cd00533524d75fd7c1e0362e7dea
Ericka Ponce <erickaponce92@gmail.com>
2024-04-23 12:37:44 -0600

    change code structure and finalize the project

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -5,8 +5,8 @@ numeroAleatorio = random.randint(1, 10)
 while True:
     numero = int(input("ingrese un número del 1 al 10: "))
 
-    if numero == numeroAleatorio:   
+    if numero != numeroAleatorio:   
+        print("Suerte para la proxima. Intentalo de nuevo")
+    else:
         print("Felicidades! Has adivinado el numero")
         break
-    else:
-        print("Suerte para la proxima. Intentalo de nuevo")

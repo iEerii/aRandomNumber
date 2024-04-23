696b4cabaeb29752434b4cc2d7d95d58f12e21b3
Ericka Ponce <erickaponce92@gmail.com>
2024-04-23 12:02:13 -0600

    create if else sentence

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,1 +1,10 @@
+import random
 
+numero = int(input("ingrese un número del 1 al 10: "))
+numeroAleatorio = random.randint(1, 10)
+
+if numero == numeroAleatorio:   
+    print("Felicidades! Has adivinado el numero")
+else:
+    print("Suerte para la proxima")
+    
